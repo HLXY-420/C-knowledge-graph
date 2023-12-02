7bd97b52db775cfe841edd17cd93864e8bd754cd
HLXY <hlxy420@outlook.com>
2023-12-03 01:06:37 +0800

    build a simple graph and add github page workflows.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), tailwind()]
+  integrations: [react(), mdx(), tailwind()],
+  site: 'https://hlxy-420.github.io',
+  base: '/c-knowledge-graph',
 });
